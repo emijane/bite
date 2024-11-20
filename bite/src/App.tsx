2593abc6570cc21cd599acdf7a374cc2e2b62e87
Emma <88465944+emijane@users.noreply.github.com>
2024-11-20 01:07:06 -0500

    added inputs for recipe search

--- bite/src/App.tsx
+++ bite/src/App.tsx
@@ -1,7 +1,20 @@
 function App() {
   return (
-    <div className="bg-blue-500 text-white text-center p-10">
-      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    <div className="wrapper">
+      <div className="bg-pink-200 text-black text-center p-10">
+        <h1 className="text-4xl font-bold">bite</h1>
+      </div>
+        <div className="body m-10 flex flex-col justify-center">
+          <div className="search flex flex-row gap-3 justify-center">
+            <input
+              name="recipe"
+              placeholder="search for a recipe"
+              className="outline-black border-2 border-black px-2 py-1"
+            />
+            <button className="bg-pink-300 text-white font-bold py-2 px-4 rounded">Search</button>
+          </div>
+          <h3 className="my-5 text-center">Recipes:</h3>
+        </div>
     </div>
   );
 }
