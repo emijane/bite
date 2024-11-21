988951ca2b7895537245a504355c612316505f95
Emma <88465944+emijane@users.noreply.github.com>
2024-11-21 17:47:58 -0500

    Added react components and installed dependencies for OpenAI for translation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,16 @@
+import Header from './components/Header';
+import SearchFields from './components/SearchFields';
+
 function App() {
-  return (
-    <div className="wrapper">
-      <div className="bg-pink-200 text-black text-center p-10">
-        <h1 className="text-4xl font-bold">bite</h1>
-      </div>
-        <div className="body m-10 flex flex-col justify-center">
-          <div className="search flex flex-row gap-3 justify-center">
-            <input
-              name="recipe"
-              placeholder="search for a recipe"
-              className="outline-black border-2 border-black px-2 py-1"
-            />
-            <button className="bg-pink-300 text-white font-bold py-2 px-4 rounded">Search</button>
+    return (
+        <div className="wrapper">
+          <Header />
+          <div className="body m-10 flex flex-col justify-center">
+            <SearchFields />
+            <h3 id="translatedRecipe" className="my-5 text-center">Translated recipe:</h3>
           </div>
-          <h3 className="my-5 text-center">Recipes:</h3>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="bg-pink-200 text-black text-center p-10">
+            <h1 className="text-4xl font-bold">bite</h1>
+        </div>
+    );
+};
+
+export default Header;
